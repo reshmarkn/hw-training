90adf0ea7a7237dd6b36e733bdb967bc5fb80259
reshmarkn <reshmarkn97@gmail.com>
2021-08-10 18:04:09 +0530

    commit

---
+++ 2021-08-10/rabbitmq.py
@@ -0,0 +1,8 @@
+import pika
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='',routing_key='hello',body='hello world')
+print("[x] send 'hello world'")
+connection.close()
